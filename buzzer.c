ff6fda342ef1dd914f26a05383759756632c3158
Aidan Colon <77650375+aidcol@users.noreply.github.com>
2024-04-09 22:27:39 -0500

    Create buzzer files

---
+++ buzzer.c
@@ -0,0 +1,10 @@
+/*
+ * buzzer.c
+ *
+ *  Created on: Apr 9, 2024
+ *      Author: aidan
+ */
+
+
+
+
---
+++ buzzer.h
@@ -0,0 +1,15 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Apr 9, 2024
+ *      Author: aidan
+ */
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+
+
+
+
+#endif /* BUZZER_H_ */
