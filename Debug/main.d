b50e4d0dc622bd6e6d3813949c3bd9a7bce5738f
maazzuberi <69701387+maazzuberi@users.noreply.github.com>
2024-04-24 11:11:30 -0500

    lcd and i2c files
    added header files/source files for Display implementation

--- Debug/main.d
+++ Debug/main.d
@@ -1,24 +1,24 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccs1260/ccs/ccs_base/msp430/include/msp430.h
-main.obj: C:/ti/ccs1260/ccs/ccs_base/msp430/include/msp430g2553.h
-main.obj: C:/ti/ccs1260/ccs/ccs_base/msp430/include/in430.h
-main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
-main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
-main.obj: C:/Users/aidan/source/repos/css/elec327-final-project/buzzer.h
+main.obj: /Applications/ti/ccs1260/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccs1260/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: /Applications/ti/ccs1260/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /Users/maazzuberi/Documents/GitHub/elec327-final-project/buzzer.h
 
 ../main.c:
 
-C:/ti/ccs1260/ccs/ccs_base/msp430/include/msp430.h:
+/Applications/ti/ccs1260/ccs/ccs_base/msp430/include/msp430.h:
 
-C:/ti/ccs1260/ccs/ccs_base/msp430/include/msp430g2553.h:
+/Applications/ti/ccs1260/ccs/ccs_base/msp430/include/msp430g2553.h:
 
-C:/ti/ccs1260/ccs/ccs_base/msp430/include/in430.h:
+/Applications/ti/ccs1260/ccs/ccs_base/msp430/include/in430.h:
 
-C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
 
-C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
 
-C:/Users/aidan/source/repos/css/elec327-final-project/buzzer.h:
+/Users/maazzuberi/Documents/GitHub/elec327-final-project/buzzer.h:
 
--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -2,34 +2,44 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-SHELL = cmd.exe
-
 # Add inputs and outputs from these tool invocations to the build variables 
 CMD_SRCS += \
 ../lnk_msp430g2553.cmd 
 
 C_SRCS += \
 ../buzzer.c \
+../i2c.c \
+../lcd.c \
 ../main.c 
 
 C_DEPS += \
 ./buzzer.d \
+./i2c.d \
+./lcd.d \
 ./main.d 
 
 OBJS += \
 ./buzzer.obj \
+./i2c.obj \
+./lcd.obj \
 ./main.obj 
 
 OBJS__QUOTED += \
 "buzzer.obj" \
+"i2c.obj" \
+"lcd.obj" \
 "main.obj" 
 
 C_DEPS__QUOTED += \
 "buzzer.d" \
+"i2c.d" \
+"lcd.d" \
 "main.d" 
 
 C_SRCS__QUOTED += \
 "../buzzer.c" \
+"../i2c.c" \
+"../lcd.c" \
 "../main.c" 
 
 
